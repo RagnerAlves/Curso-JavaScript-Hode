3303607ce78a3d86ccf2903b322c18d296ca0737
Ragner Alves <ragnerf@hotmail.com>
2020-05-05 14:06:23 -0300

    Metodo Constructor e Encaps.

--- Calculadora/scripts/controller/CalcController.js
+++ Calculadora/scripts/controller/CalcController.js
@@ -1,3 +1,29 @@
 class CalcController    {
-    
+
+    constructor()   {
+
+        this._displayCalc = "0" //atributos privatos sao declarados com "_"
+        this._currentDate
+    }
+
+    get displayCalc()   {
+
+        return this._displayCalc
+    }
+
+    set displayCalc(valor)   {
+
+        this._displayCalc = valor
+    }
+
+    get currentDate()   {
+
+        return this._currentDate
+    }
+
+    set currentDate(valor)   {
+        
+        this._currentDate = valor
+    }
+
 }
