9212cc72e37d94a18ab0cd83faf2b8e2423bf3eb
Ragner Alves <ragnerf@hotmail.com>
2020-05-05 14:32:49 -0300

    Manipulando o DOM

--- Calculadora/scripts/controller/CalcController.js
+++ Calculadora/scripts/controller/CalcController.js
@@ -4,6 +4,19 @@ class CalcController    {
 
         this._displayCalc = "0" //atributos privatos sao declarados com "_"
         this._currentDate
+        this.initialize()
+    }
+
+    initialize()    {
+
+        let displayCalcEl = document.querySelector('#display')
+        let dateEl = document.querySelector('#data')
+        let timeEl = document.querySelector('#hora')
+
+        displayCalcEl.innerHTML = '4567';
+        dateEl.innerHTML = '05/05/2020';
+        timeEl.innerHTML = "14:25";
+        
     }
 
     get displayCalc()   {
