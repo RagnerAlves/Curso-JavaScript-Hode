9ca6e30d22cefa0dd3fbfa8ab4c86b59161cf7ed
Ragner Alves <ragnerf@hotmail.com>
2020-06-16 16:05:33 -0300

    API File Reader

--- Ger. Usuarios MVC/controllers/UserController.js
+++ Ger. Usuarios MVC/controllers/UserController.js
@@ -15,27 +15,55 @@ class UserController    {
 
             event.preventDefault()
 
-            this.addLine(this.getValues)
+            let values = this.getValue()
+
+            values.photo= ""
+            
+            this.getPhoto((content) =>{
+
+                value.photo = content
+                this.addLine(value)
+            })
+            
         })
     }    
 // close onSubmit
+
+
+    getPhoto(callback)  {
+
+        let fileReader= new FileReader()
+
+        let elements = [...this.formEl.elements].filter(item => {
+
+            if (item.name == 'photo'){
+                return item 
+            }
+        })
+
+        let file = elements[0].files[0]
+
+        fileReader.onload = () => {
+
+            callback(fileReader.result)
+        }
+
+        fileReader.readAsDataURL(file)
+    }
 
     getvalues() {
 
         let user = {}
 
-        this.formEl.elements
+        (...this.formEl.elements).forEach(function(field, index){
 
-        this.formEl.elements.forEach(function(field, index){
-
-            if (field.name === "gender" && field.checked == true)   {
+            if (field.name === "gender")   {
                 user[field.name] = field.value
                    
             } else {
         
                 user[field.name] = field.value
-            }
-        
+            }        
         })
     
         return new User(
@@ -56,7 +84,7 @@ class UserController    {
         
         this.tableEl.innerHTML =
         `<tr>
-            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin}</td>
