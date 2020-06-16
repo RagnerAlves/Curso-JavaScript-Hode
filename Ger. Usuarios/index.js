90ff2865d11881cc77195a95bc29e75746e5c90f
Ragner Alves <ragnerf@hotmail.com>
2020-06-16 08:56:47 -0300

    Trab. JSON

--- Ger. Usuarios/index.js
+++ Ger. Usuarios/index.js
@@ -1,1 +1,16 @@
-alert("hello Wolrd !! ")
+var fields = document.querySelectorAll("#form-user-create [name]")
+var user = {}
+
+fields.forEach(function(field, index){
+
+    if (field.name === "gender" && field.checked == true)   {
+        user[field.name] = field.value
+           
+    } else {
+
+        user[field.name] = field.value
+    }
+
+    //console.log(field.id, field.name, field.nodeValue, field.checked, index)
+
+})
