435bb6669053f68db625d69d90889647629a333d
Ragner Alves <ragnerf@hotmail.com>
2020-06-29 15:41:35 -0300

    Fnct Excluir

--- JavaScript_Basico/todoos.js
+++ JavaScript_Basico/todoos.js
@@ -1,5 +1,5 @@
 var listElement = document.querySelector('#app ul')
-var inptElement = document.querySelector('#app input')
+var inputElement = document.querySelector('#app input')
 var buttonElement = document.querySelector('#app button')
 
 var todoos = [
@@ -9,12 +9,47 @@ var todoos = [
 ]
 
 function renderTodoos() {
-    for (todoo of todoos)   {
 
+    listElement.innerHTML = ''
+
+    for (todoo of todoos)   {
         var todooElement = document.createElement('li')
         var todooText = document.createTextNode(todoo)
 
+        var linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        var linkText = document.createTextNode('Excluir')
+
+        linkElement.appendChild(linkText)
+
+        var pos = todoo.indexOf(todoo)
+
+        linkElement.setAttribute('onclick', 'deleteTodoo('+ pos +')')
+        
         todooElement.appendChild(todooText)
+        todooElement.appendChild(linkElement)
+
         listElement.appendChild(todooElement)
     }
+}
+
+renderTodoos()
+
+function addTodoo() {
+
+    var todooText = inputElement.value
+
+    todoos.push(todooText)
+        inputElement.value = ''
+        renderTodoos()
+}
+
+buttonElement.onclick = addTodoo
+
+
+function deleteTodoo(pos)  {
+
+    todoo.splice(pos, 1)
+    renderTodoos()
 }
