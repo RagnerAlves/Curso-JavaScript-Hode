2b810398ba61cf5a956f06efab7386303fef507a
Ragner Alves <ragnerf@hotmail.com>
2020-06-29 16:19:38 -0300

    PromiseJS

--- JavaScript_Basico/Introd_Ajax/main.js
+++ JavaScript_Basico/Introd_Ajax/main.js
@@ -1,12 +1,40 @@
-var xhr = new XMLHttpRequest()
+// var xhr = new XMLHttpRequest()
 
-xhr.open('GET', 'https://api.github.com/users')
-xhr.send(null)
+// xhr.open('GET', 'https://api.github.com/users')
+// xhr.send(null)
 
-xhr.onreadystatechange = function() {
+// xhr.onreadystatechange = function() {
 
-    if (xhr.readyState == 4)    {
+//     if (xhr.readyState == 4)    {
 
-        console.log(JSON.parse(xhr.responseText)) // pois o retorno é um JSOn assim converto em objt em JSON.parse
-    }
+//         console.log(JSON.parse(xhr.responseText)) // pois o retorno é um JSOn assim converto em objt em JSON.parse
+//     }
+// }
+
+var minhaPromise = function()   { 
+
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest()
+        xhr.open('GET', 'https://api.github.com/users')
+        xhr.send(null)
+
+        xhr.onreadystatechange = function() {
+
+            if (xhr.readyState == 4) {
+                if(xhr.responseText == 200) {
+                    resolve(JSON.parse(xhr.responseText))
+                }   else{
+                    reject('Erro na requisição')
+                }
+            }
+        }
+    })
 }
+
+minhaPromise()
+    .then(function(response){
+        console.log(response)
+    })
+    .catch(function(error){
+        console.warn(error)
+    })
