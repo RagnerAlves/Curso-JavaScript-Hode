ecf60d4fb28887b54ef3ca6625a604435c8a9dd8
Ragner Alves <ragnerf@hotmail.com>
2020-06-29 15:09:22 -0300

    Iniciando app

--- JavaScript_Basico/todoos.js
+++ JavaScript_Basico/todoos.js
@@ -1,1 +1,9 @@
-alert('Funcionou !')
+var listElement = document.querySelector('#app ul')
+var inptElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+
+var todoos = [
+    'Fazer café',
+    'Estudar C#',
+    'Acessar Comunidade'
+]
